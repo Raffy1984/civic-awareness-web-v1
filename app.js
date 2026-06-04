060f120469ad6797aa7308295a4bb4daaa576cf8
Raffy1984 <raffaelericci@hotmail.it>
2026-06-04 14:40:40 +0200

    app.js

--- app.js
+++ app.js
@@ -1,8 +1,31 @@
 document.addEventListener("DOMContentLoaded", () => {
+
   const btn = document.getElementById("startBtn");
   const output = document.getElementById("output");
 
   btn.addEventListener("click", () => {
-    output.innerHTML = "<h2>FUNZIONA ✔</h2>";
+
+    output.innerHTML = `
+      <h2>Domanda 1</h2>
+
+      <p>Quale di queste è un'istituzione dell'Unione Europea?</p>
+
+      <button onclick="rispostaCorretta()">Parlamento Europeo</button>
+
+      <button onclick="rispostaErrata()">Ferrari</button>
+
+      <button onclick="rispostaErrata()">Protezione Civile</button>
+
+      <button onclick="rispostaErrata()">INPS</button>
+    `;
   });
+
 });
+
+function rispostaCorretta() {
+  alert("Risposta corretta!");
+}
+
+function rispostaErrata() {
+  alert("Risposta errata!");
+}
