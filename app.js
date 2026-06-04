1954a5edb0a08077c9423e3be8c2d3bbdb8d279a
Raffy1984 <raffaelericci@hotmail.it>
2026-06-04 14:27:32 +0200

    app.js

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const btn = document.getElementById("startBtn");
+  const output = document.getElementById("output");
 
+  btn.addEventListener("click", () => {
+    output.innerHTML = "<h2>FUNZIONA ✔</h2>";
+  });
+});
