447cf7604f453d0b69a39dd4580001f856a50955
Raffy1984 <raffaelericci@hotmail.it>
2026-06-04 14:16:43 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   const btn = document.getElementById("startBtn");
 
-  if (!btn) {
-    alert("Bottone non trovato");
-    return;
-  }
-
   btn.addEventListener("click", () => {
-    alert("START APP FUNZIONA");
+    alert("OK: bottone funzionante");
   });
 });
