60eec5f468b4586219dc8927122c37c4dfe04310
Raffy1984 <raffaelericci@hotmail.it>
2026-06-04 14:49:46 +0200

    app.js

--- app.js
+++ app.js
@@ -1,25 +1,17 @@
-document.addEventListener("DOMContentLoaded", () => {
+const btn = document.getElementById("startBtn");
+const output = document.getElementById("output");
 
-  const btn = document.getElementById("startBtn");
-  const output = document.getElementById("output");
+btn.addEventListener("click", () => {
+  output.innerHTML = `
+    <h2>Domanda 1</h2>
 
-  btn.addEventListener("click", () => {
+    <p>Quale di queste è un'istituzione dell'Unione Europea?</p>
 
-    output.innerHTML = `
-      <h2>Domanda 1</h2>
-
-      <p>Quale di queste è un'istituzione dell'Unione Europea?</p>
-
-      <button onclick="rispostaCorretta()">Parlamento Europeo</button>
-
-      <button onclick="rispostaErrata()">Ferrari</button>
-
-      <button onclick="rispostaErrata()">Protezione Civile</button>
-
-      <button onclick="rispostaErrata()">INPS</button>
-    `;
-  });
-
+    <button onclick="rispostaCorretta()">Parlamento Europeo</button>
+    <button onclick="rispostaErrata()">Ferrari</button>
+    <button onclick="rispostaErrata()">Protezione Civile</button>
+    <button onclick="rispostaErrata()">INPS</button>
+  `;
 });
 
 function rispostaCorretta() {
