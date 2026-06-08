c8271b37b5ac0e1e7f5e408855f1255545c14553
Raffy1984 <raffaelericci@hotmail.it>
2026-06-08 23:19:57 +0200

    territori.js

--- territori.js
+++ territori.js
@@ -1,1 +1,6 @@
-
+const REGIONI = [
+  "Abruzzo","Basilicata","Calabria","Campania","Emilia-Romagna",
+  "Friuli Venezia Giulia","Lazio","Liguria","Lombardia","Marche",
+  "Molise","Piemonte","Puglia","Sardegna","Sicilia","Toscana",
+  "Trentino-Alto Adige","Umbria","Valle d'Aosta","Veneto"
+];
